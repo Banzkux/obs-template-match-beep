507a400af1c788b4b58be3c039a23080fd074545
Banzkux <21155859+Banzkux@users.noreply.github.com>
2022-12-19 20:28:10 +0200

    Added sources from broken repo (FrameIngest and template-match-beep)

---
+++ src/FrameIngest.hpp
@@ -0,0 +1,210 @@
+//    *************************** LiveVisionKit ****************************
+//    Copyright (C) 2022  Sebastian Di Marco (crowsinc.dev@gmail.com)
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// 	  **********************************************************************
+
+#pragma once
+
+#include <memory>
+#include <obs-module.h>
+#include <opencv2/opencv.hpp>
+
+namespace lvk
+{
+
+	class FrameIngest
+	{
+	public:
+		
+		static std::unique_ptr<FrameIngest> Select(video_format format);
+
+		virtual ~FrameIngest() = default;
+
+		// Uploads the src obs_source_frame to the dst YUV UMat on the GPU
+		virtual void upload(const obs_source_frame* src, cv::UMat& dst) = 0;
+
+		// Downloads the src YUV UMat to the dst obs_source_frame, preserving any existing alpha channels
+		virtual void download(const cv::UMat& src, obs_source_frame* dst) = 0;
+
+		video_format format();
+
+	protected:
+
+		explicit FrameIngest(video_format format);
+
+		// NOTE: returns ROI to internal buffers
+		cv::UMat upload_planes(
+			const obs_source_frame& src,
+			const uint32_t channels
+		);
+
+		// NOTE: returns ROI to internal buffers
+		cv::UMat upload_planes(
+			const obs_source_frame& src,
+			const cv::Size plane_0_size,
+			const uint32_t plane_0_channels
+		);
+		
+		// NOTE: returns ROI to internal buffers
+		std::tuple<cv::UMat, cv::UMat> upload_planes(
+			const obs_source_frame& src,
+			const cv::Size plane_0_size,
+			const uint32_t plane_0_channels,
+			const cv::Size plane_1_size,
+			const uint32_t plane_1_channels
+		);
+		
+		// NOTE: returns ROI to internal buffers
+		std::tuple<cv::UMat, cv::UMat, cv::UMat> upload_planes(
+			const obs_source_frame& src,
+			const cv::Size plane_0_size,
+			const uint32_t plane_0_channels,
+			const cv::Size plane_1_size,
+			const uint32_t plane_1_channels,
+			const cv::Size plane_2_size,
+			const uint32_t plane_2_channels
+		);
+
+		void download_planes(
+			const cv::UMat& plane_0,
+			obs_source_frame& dst
+		);
+		
+		void download_planes(
+			const cv::UMat& plane_0,
+			const cv::UMat& plane_1,
+			obs_source_frame& dst
+		);
+		
+		void download_planes(
+			const cv::UMat& plane_0,
+			const cv::UMat& plane_1,
+			const cv::UMat& plane_2,
+			obs_source_frame& dst
+		);
+
+		static void fill_plane(obs_source_frame& dst, const uint32_t plane, const uint8_t value);
+
+		static void merge_planes(const cv::UMat& p1, const cv::UMat& p2, const cv::UMat& p3, cv::UMat& dst);
+
+		static void merge_planes(const cv::UMat& p1, const cv::UMat& p2, cv::UMat& dst);
+
+		static void split_planes(const cv::UMat& src, cv::UMat& p1, cv::UMat& p2, cv::UMat& p3);
+
+		static void split_planes(const cv::UMat& src, cv::UMat& p1, cv::UMat& p2);
+
+		static bool test_obs_frame(const obs_source_frame* frame);
+
+	private:
+		video_format m_Format = VIDEO_FORMAT_NONE;
+	
+		cv::UMat m_ImportBuffer{cv::UMatUsageFlags::USAGE_ALLOCATE_DEVICE_MEMORY};
+		cv::UMat m_ExportBuffer{cv::UMatUsageFlags::USAGE_ALLOCATE_DEVICE_MEMORY};
+	};
+
+
+	// Planar 4xx formats
+	class I4XXIngest : public FrameIngest
+	{
+	public:
+
+		explicit I4XXIngest(video_format i4xx_format);
+
+		void upload(const obs_source_frame* src, cv::UMat& dst) override;
+	
+		void download(const cv::UMat& src, obs_source_frame* dst) override;
+
+	private:
+		cv::Size2f m_ChromaScaling;
+		
+		// NOTE: We assume this will automatically initialize on the GPU
+		cv::UMat m_YPlane, m_UPlane, m_VPlane;
+		cv::UMat m_USubPlane, m_VSubPlane;
+	};
+	
+	// Semi-planar NV12 format
+	class NV12Ingest : public FrameIngest
+	{
+	public:
+
+		NV12Ingest();
+
+        void upload(const obs_source_frame* src, cv::UMat& dst) override;
+		
+		void download(const cv::UMat& src, obs_source_frame* dst) override;
+
+	private:
+		// NOTE: We assume this will automatically initialize on the GPU
+		cv::UMat m_YPlane, m_UVPlane, m_UVSubPlane;
+	};
+
+	// Packed 422 formats
+	class P422Ingest : public FrameIngest
+	{
+	public:
+
+		explicit P422Ingest(video_format packed_422_format);
+
+		void upload(const obs_source_frame* src, cv::UMat& dst) override;
+		
+		void download(const cv::UMat& src, obs_source_frame* dst) override;
+	
+	private:
+		bool m_YFirst, m_UFirst;
+		
+		// NOTE: We assume this will automatically initialize on the GPU
+		cv::UMat m_YPlane, m_UVPlane, m_UVSubPlane, m_MixBuffer;
+	};
+
+	// Packed 444 formats
+	class P444Ingest : public FrameIngest
+	{
+	public:
+
+		P444Ingest();
+
+		void upload(const obs_source_frame* src, cv::UMat& dst) override;
+		
+		void download(const cv::UMat& src, obs_source_frame* dst) override;
+
+	private:
+		// NOTE: We assume this will automatically initialize on the GPU
+		cv::UMat m_MixBuffer;
+	};
+
+	// Uncompressed formats
+	class DirectIngest : public FrameIngest
+	{
+	public:
+
+		explicit DirectIngest(video_format uncompressed_format);
+
+		void upload(const obs_source_frame* src, cv::UMat& dst) override;
+
+		void download(const cv::UMat& src, obs_source_frame* dst) override;
+
+	private:
+		int m_Components;
+		bool m_SteppedConversion;
+		cv::ColorConversionCodes m_ForwardConversion, m_ForwardStepConversion;
+		cv::ColorConversionCodes m_BackwardConversion, m_BackwardStepConversion;
+
+		// NOTE: We assume this will automatically initialize on the GPU
+		cv::UMat m_ConversionBuffer{cv::UMatUsageFlags::USAGE_ALLOCATE_DEVICE_MEMORY};
+        cv::UMat m_StepConversionBuffer{cv::UMatUsageFlags::USAGE_ALLOCATE_DEVICE_MEMORY};
+	};
+
+}
+
---
+++ src/template-match-beep.cpp
@@ -0,0 +1,367 @@
+/*
+OBS Template Match Beep
+Copyright (C) 2022 Janne Pitkanen <acebanzkux@gmail.com>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program. If not, see <https://www.gnu.org/licenses/>
+*/
+
+#include <obs-module.h>
+// Remember to bundle with the opencv binaries!
+#include <opencv2/opencv.hpp>
+#include <string>
+#include <chrono>
+#include <thread>
+#include <math.h>
+#include "FrameIngest.hpp"
+
+#include "template-match-beep.generated.h"
+
+void beep(int freq, int duration);
+#ifdef _WIN32
+#include <Windows.h>
+void beep(int freq, int duration)
+{
+	Beep(freq, duration);
+}
+#elif __linux__
+#include <stdio.h>
+void beep(int freq, int duration)
+{
+	system("echo -e "\007 " >/dev/tty10");
+}
+#endif
+
+OBS_DECLARE_MODULE()
+OBS_MODULE_USE_DEFAULT_LOCALE(PLUGIN_NAME, "en-US")
+
+#ifndef SEC_TO_NSEC
+#define SEC_TO_NSEC 1000000000ULL
+#endif
+
+#ifndef MSEC_TO_SEC
+#define MSEC_TO_SEC 0.001
+#endif
+
+#define SETTING_DELAY_MS "delay_ms"
+#define SETTING_COOLDOWN_MS "cooldown_ms"
+#define SETTING_PATH "template_path"
+#define SETTING_STATUS "status"
+
+#define TEXT_DELAY_MS obs_module_text("Timerr")
+#define TEXT_COOLDOWN_MS obs_module_text("Cooldown timer")
+#define TEXT_PATH obs_module_text("Template image path")
+#define TEXT_STATUS std::string("Status: ")
+
+struct template_match_beep_data {
+	obs_source_t *context;
+
+	/* contains struct obs_source_frame* */
+	obs_source_frame *current_frame;
+
+	// Rest of the needed variables here (I guess)
+	uint64_t last_video_ts;
+	uint64_t timer;
+	uint64_t cooldown_timer;
+	const char *path;
+	std::string state;
+	bool timer_active;
+	uint64_t timer_video_ts;
+	std::thread thread;
+	bool thread_active;
+	cv::Mat template_image;
+	cv::UMat current_cv_frame;
+
+	std::unique_ptr<lvk::FrameIngest> frame_ingest;
+};
+
+static const char *template_match_beep_filter_name(void *unused)
+{
+	UNUSED_PARAMETER(unused);
+	return obs_module_text("Template Match Timer");
+}
+
+// Filters settings were updated
+static void template_match_beep_filter_update(void *data, obs_data_t *settings)
+{
+	struct template_match_beep_data *filter = (template_match_beep_data*)data;
+	uint64_t new_timer =
+		(uint64_t)obs_data_get_int(settings, SETTING_DELAY_MS);
+
+	uint64_t new_cooldown =
+		(uint64_t)obs_data_get_int(settings, SETTING_COOLDOWN_MS);
+
+	const char *new_path =
+		(const char *)obs_data_get_string(settings, SETTING_PATH);
+
+	filter->timer = new_timer;
+	filter->cooldown_timer = new_cooldown;
+	// Update template image on new path
+	if (filter->path != new_path) {
+		filter->path = new_path;
+		filter->template_image = cv::imread(new_path, 0);
+	}
+
+	blog(LOG_INFO, "asd");
+}
+
+void thread_loop(void *data);
+
+static void *template_match_beep_filter_create(obs_data_t *settings,
+				       obs_source_t *context)
+{
+	struct template_match_beep_data *filter =
+		(template_match_beep_data *)bzalloc(sizeof(*filter));
+
+	filter->context = context;
+	template_match_beep_filter_update(filter, settings);
+
+	filter->current_frame = nullptr;
+	filter->state = TEXT_STATUS + "Created";
+	filter->timer_video_ts = 0;
+	filter->thread_active = true;
+	filter->thread = std::thread(thread_loop, (void *)filter);
+	return filter;
+}
+
+static void template_match_beep_filter_destroy(void *data)
+{
+	struct template_match_beep_data *filter =
+		(template_match_beep_data *)data;
+
+	filter->thread_active = false;
+
+	bfree(data);
+}
+
+// calibrate button was clicked
+bool template_match_beep_calibrate(obs_properties_t *props,
+				    obs_property*, void* data)
+{
+	struct template_match_beep_data *filter =
+		(template_match_beep_data *)data;
+
+	// Do calibration
+	filter->state = TEXT_STATUS + "Calibration";
+	obs_property_set_description(obs_properties_get(props, SETTING_STATUS),
+				     filter->state.c_str());
+	// Check if the filter is active
+	if (obs_source_enabled(filter->context))
+		blog(LOG_INFO, "calibrate pressed");
+
+	return true;
+}
+
+// reset calibration button was clicked
+bool template_match_beep_reset_calibration(obs_properties_t *props, obs_property *,
+				    void *data)
+{
+	struct template_match_beep_data *filter =
+		(template_match_beep_data *)data;
+
+	// Reset calibration
+	filter->state = TEXT_STATUS + "Reset calibration";
+	obs_property_set_description(obs_properties_get(props, SETTING_STATUS),
+				     filter->state.c_str());
+	// Check if the filter is active
+	if (obs_source_enabled(filter->context))
+		blog(LOG_INFO, "reset pressed");
+
+	return true;
+}
+
+static obs_properties_t *template_match_beep_filter_properties(void *data)
+{
+	struct template_match_beep_data *filter =
+		(template_match_beep_data *)data;
+
+	obs_properties_t *props = obs_properties_create();
+
+	obs_properties_add_text(props, SETTING_STATUS, filter->state.c_str(),
+				OBS_TEXT_INFO);
+
+	obs_property_t *p = obs_properties_add_int(props, SETTING_DELAY_MS,
+						   TEXT_DELAY_MS, 0, 20000, 1);
+	obs_property_int_set_suffix(p, " ms");
+
+	obs_property_t *c = obs_properties_add_int(props, SETTING_COOLDOWN_MS,
+						   TEXT_COOLDOWN_MS, 0, 20000, 1);
+	obs_property_int_set_suffix(c, " ms");
+
+	// obs_properties_add_path
+	// obs_properties_add_button
+
+	obs_properties_add_path(props, SETTING_PATH, TEXT_PATH, OBS_PATH_FILE,
+				"*.png", NULL);
+
+	obs_properties_add_button(props, "calibrate", "Calibrate",
+					  template_match_beep_calibrate);
+	obs_properties_add_button(props, "reset_calibration", "Reset Calibration",
+				  template_match_beep_reset_calibration);
+
+	return props;
+}
+
+static void template_match_beep_filter_remove(void *, obs_source_t *)
+{
+	blog(LOG_INFO, "filter removed");
+}
+
+// https://blat-blatnik.github.io/computerBear/making-accurate-sleep-function/
+// ^^ also has windows only implementation for better cpu usage with slightly lower accuracy
+void preciseSleep(double seconds)
+{
+	using namespace std;
+	using namespace std::chrono;
+
+	static double estimate = 5e-3;
+	static double mean = 5e-3;
+	static double m2 = 0;
+	static int64_t count = 1;
+
+	while (seconds > estimate) {
+		auto start = high_resolution_clock::now();
+		this_thread::sleep_for(milliseconds(1));
+		auto end = high_resolution_clock::now();
+
+		double observed = (end - start).count() / 1e9;
+		seconds -= observed;
+
+		++count;
+		double delta = observed - mean;
+		mean += delta / count;
+		m2 += delta * (observed - mean);
+		double stddev = sqrt(m2 / (count - 1));
+		estimate = mean + stddev;
+	}
+
+	// spin lock
+	auto start = high_resolution_clock::now();
+	while ((high_resolution_clock::now() - start).count() / 1e9 < seconds)
+		;
+}
+
+// Thread for template matching and beeping
+void thread_loop(void* data) {
+	struct template_match_beep_data *filter =
+		(template_match_beep_data *)data;
+
+	obs_source_frame *frame = nullptr;
+
+	while (filter->thread_active) {
+		if (frame != filter->current_frame && !filter->template_image.empty() && filter->frame_ingest) {
+			frame = filter->current_frame;
+			cv::UMat umat, umat2;
+			filter->frame_ingest->upload(frame, umat);
+			// Changing color format, this may become issue
+			cv::cvtColor(umat, umat2, cv::COLOR_YUV2BGR, 4);
+			// Gray
+			cv::cvtColor(umat2, filter->current_cv_frame,
+				     cv::COLOR_BGR2GRAY);
+			cv::Mat result;
+			int result_cols = filter->current_cv_frame.cols -
+					  filter->template_image.cols + 1;
+			int result_rows = filter->current_cv_frame.rows -
+					  filter->template_image.rows + 1;
+			result.create(result_rows, result_cols, CV_32FC1);
+
+			cv::matchTemplate(filter->current_cv_frame,
+					  filter->template_image, result,
+					  cv::TM_CCOEFF_NORMED);
+
+			double minVal, maxVal;
+			cv::Point minLoc, maxLoc, matchLoc;
+			cv::minMaxLoc(result, &minVal, &maxVal, &minLoc,
+				      &maxLoc, cv::Mat());
+			matchLoc = maxLoc;
+
+			cv::threshold(result, result, 0.80, 1.0,
+				      cv::THRESH_TOZERO);
+			// Detected template image!
+			if (maxVal > 0.8) {
+				cv::rectangle(
+					filter->current_cv_frame, matchLoc,
+					cv::Point(matchLoc.x +
+							  filter->template_image
+								  .cols,
+						  matchLoc.y +
+							  filter->template_image
+								  .rows),
+					cv::Scalar(255), 2, 8, 0);
+
+				// Detection beep
+				// NOTE: the beep is synchronous!
+				beep(880, 100);
+				preciseSleep(filter->timer * MSEC_TO_SEC);
+				// Beep after timer
+				beep(440, 100);
+				// Cooldown time until next template match
+				preciseSleep(filter->cooldown_timer *
+					     MSEC_TO_SEC);
+			}
+		}  else {
+			// chill for a bit
+			std::this_thread::sleep_for(std::chrono::milliseconds(1));
+		}
+	}
+}
+
+// Filter gets a frame
+static struct obs_source_frame *
+template_match_beep_filter_video(void *data, struct obs_source_frame *frame)
+{
+	struct template_match_beep_data *filter =
+		(template_match_beep_data *)data;
+
+	filter->current_frame = frame;
+
+	if (!filter->frame_ingest ||
+	    filter->frame_ingest->format() != frame->format)
+		filter->frame_ingest = lvk::FrameIngest::Select(frame->format);
+
+	// Add property for debug or somehting
+	if (!filter->current_cv_frame.empty())
+		cv::imshow("TEST", filter->current_cv_frame);
+
+	return frame;
+}
+
+bool obs_module_load(void)
+{
+	struct obs_source_info template_match_beep_filter = {};
+	template_match_beep_filter.id = "template_match_beep_filter";
+	template_match_beep_filter.type = OBS_SOURCE_TYPE_FILTER,
+	template_match_beep_filter.output_flags = OBS_SOURCE_VIDEO |
+						   OBS_SOURCE_ASYNC;
+	template_match_beep_filter.get_name = template_match_beep_filter_name;
+	template_match_beep_filter.create = template_match_beep_filter_create;
+	template_match_beep_filter.destroy =
+		template_match_beep_filter_destroy;
+	template_match_beep_filter.update = template_match_beep_filter_update,
+	template_match_beep_filter.get_properties =
+		template_match_beep_filter_properties;
+	template_match_beep_filter.filter_video =
+		template_match_beep_filter_video;
+	template_match_beep_filter.filter_remove =
+		template_match_beep_filter_remove;
+
+	obs_register_source(&template_match_beep_filter);
+	blog(LOG_INFO, "plugin loaded successfully (version %s)",
+	     PLUGIN_VERSION);
+	return true;
+}
+
+void obs_module_unload()
+{
+	blog(LOG_INFO, "plugin unloaded");
+}
